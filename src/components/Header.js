989b62d9d6f5faa28ceead4142fdc3d8223c4b4f
winfredo <winfrednukpe2002@gmail.com>
2024-01-16 15:09:04 +0000

    Updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,9 +11,9 @@ const Header = () => {
       <div className=" h-full flex lg:max-w-[1250px]   mx-auto  ">
           <div className="flex flex-1 items-center justify-between">
           <img
-            src="./images/logo-no-background.png"
+            src="./images/Logo.png"
             alt="logo"
-            className=" h-[50px] mr-10 ml-5 w-[170px]"
+            className=" mr-10 ml-5 "
           />
 
             <div className="hidden lg:flex flex-1 items-center justify-between">
@@ -22,7 +22,7 @@ const Header = () => {
                   placeholder="Search"
                   className="w-[300px] h-[35px] rounded-md outline-0 border-0 pl-2 mr-4 bg-[#F5F5F5]"
                 />
-                <ul className="flex items-center  space-x-12">
+                <ul className="flex items-center justify-between px-10 flex-1 space-x-12">
                   <li className="text-md text-[#b2b2b2] cursor-pointer ">Home</li>
                   <li className="text-md text-[#b2b2b2] cursor-pointer ">About</li>
                   <li className="text-md text-[#b2b2b2]  cursor-pointer">
