5919bf404ad338575d3d62c3e4bef31a46feb351
winfredo <winfrednukpe2002@gmail.com>
2024-01-18 13:01:01 +0000

    Updates

--- src/components/pages/Products.js
+++ src/components/pages/Products.js
@@ -1,21 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../Header";
 import SubHeader from "../SubHeader";
 import { LiaGreaterThanSolid } from "react-icons/lia";
-import { InputNumber, Space } from 'antd';
+import { InputNumber } from "antd";
+import { Slider } from "antd";
+import "./index.css";
 
 const Products = () => {
-
-  
+  const [inputValue1, setInputValue1] = useState(40);
+  const [inputValue2, setInputValue2] = useState(30);
   return (
-    
     <div>
       <Header />
       <SubHeader />
 
       <div className="w-full h-[5rem]  ">
         <div className="max-w-[1250px]  h-full mx-auto">
-          <div className="hidden   mx-5  lg:flex flex-1 pt-7 text-[#a4a4a4] text-sm font-light">
+          <div className="hidden mx-5 lg:flex flex-1 pt-7 text-[#a4a4a4] text-sm font-light">
             <p className="flex items-center justify-center mr-10">
               Home <LiaGreaterThanSolid className="ml-10" />
             </p>
@@ -35,26 +36,54 @@ const Products = () => {
 
         <div className=" h-[80rem] w-full">
           <div className="lg:max-w-[1250px] h-full  bg-green-300 pt-5 mx-auto">
-            <div className="w-[150px] h-full bg-white ml-5">
-              <div className="flex justify-between items-center text-[14px] pb-1 border-b border-[#b5b5b5]">
+            <div className="w-[230px] h-full bg-white ml-5">
+              <div className="flex justify-between items-center text-[14px] pb-3 border-b border-[#b5b5b5]">
                 <p className="">Price</p>
                 <p>
                   <LiaGreaterThanSolid />
                 </p>
               </div>
 
-              <div className="flex justify-between items-center text-[10px] text-[#b5b5b5]">
+              <div className="flex justify-between py-3 items-center text-[12px] text-[#b5b5b5]">
                 <p>From</p>
                 <p>To</p>
               </div>
-              
-              <InputNumber size="small" min={1} max={100000}  />
 
+              <div className="flex justify-between">
+                <InputNumber
+                  size="large"
+                  min={0}
+                  max={5000}
+                  className="rounded-[4px]"
+                  value={inputValue1}
+                  onChange={setInputValue1}
+                />
+                <p className="pt-2 text-[#a4a4a4] ">-</p>
+                <InputNumber
+                  size="large"
+                  min={0}
+                  max={5000}
+                  className="rounded-[4px]"
+                  value={inputValue2}
+                  onChange={setInputValue2}
+                />
+              </div>
+              <Slider
+                max={5000}
+                range={{
+                  draggableTrack: true,
+                }}
+                value={[inputValue1, inputValue2]}
+                onChange={(values) => {
+                  setInputValue1(values[0]);
+                  setInputValue2(values[1]);
+                }}
+              />
             </div>
+            {/* this is where the div for the whole thing ends */}
           </div>
         </div>
       </div>
-      
     </div>
   );
 };
