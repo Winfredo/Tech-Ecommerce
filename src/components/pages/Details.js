5f6a1ea452890cee7e3e4a095510ef836eec25b2
winfredo <winfrednukpe2002@gmail.com>
2024-05-06 17:32:26 +0000

    changes

--- src/components/pages/Details.js
+++ src/components/pages/Details.js
@@ -13,13 +13,14 @@ import { TbCpu2 } from "react-icons/tb";
 import { PiTruck } from "react-icons/pi";
 import { BsHouseCheck } from "react-icons/bs";
 import { ImCheckmark } from "react-icons/im";
+import { MdOutlineArrowDropDown } from "react-icons/md";
 
 const Details = () => {
   const Features = ({ icon, text1, text2 }) => {
     return (
-      <div className="h-[60px] w-[170px] rounded-md bg-[#f4f4f4] flex items-center pl-5 mb-3">
+      <div className="h-[60px] w-[45%] lg:w-[170px] rounded-md bg-[#f4f4f4] flex items-center pl-5 mb-3">
         {icon}
-        <div className="ml-3 flex flex-col text-sm">
+        <div className="ml-3 flex flex-col lg:text-sm text-[12px]">
           <p className="text-[#a7a7a7]">{text1}</p>
           <p className="text-[#4e4e4e]">{text2}</p>
         </div>
@@ -36,6 +37,22 @@ const Details = () => {
         <div className="ml-3 flex flex-col text-sm">
           <p className="text-[#717171] text-[12px]">{text1}</p>
           <p className="text-[#000] text-[12px]">{text2}</p>
+        </div>
+      </div>
+    );
+  };
+
+  const Screens = ({ text1, text2, text3, text4, text5, text6, header }) => {
+    return (
+      <div className="flex justify-between pb-1 mb-5 border-b border-opacity-40 border-[#cdcdcd]">
+        <p className="text-[13px]">{text1}</p>
+
+        <div className="text-right">
+          <p className="text-[13px]">{text2} </p>
+          <p className="text-[13px]">{text3} </p>
+          <p className="text-[13px]">{text4} </p>
+          <p className="text-[13px]">{text5} </p>
+          <p className="text-[13px]">{text6} </p>
         </div>
       </div>
     );
@@ -64,12 +81,10 @@ const Details = () => {
               Catalog <LiaGreaterThanSolid className="ml-5" />
             </p>
             <p className="text-[#a4a4a4] font-light flex items-center justify-center mr-5">
-              {" "}
-              Smartphones <LiaGreaterThanSolid className="ml-5" />{" "}
+              Smartphones <LiaGreaterThanSolid className="ml-5" />
             </p>
             <p className="text-[#a4a4a4] font-light flex items-center justify-center mr-5">
-              {" "}
-              Apple <LiaGreaterThanSolid className="ml-5" />{" "}
+              Apple <LiaGreaterThanSolid className="ml-5" />
             </p>
             <p className="text-[#a4a4a4] font-light "> iPhone 14 Pro Max</p>
           </div>
@@ -81,10 +96,10 @@ const Details = () => {
           <img
             src="./images/iphone14pro.png"
             alt="iphone14pro"
-            className="w-[50%] md:w-[35%] "
+            className="w-[50%] md:w-[35%] mb-7 md:mb-0"
           />
 
-          <div className="flex flex-col gap-3 md:w-[43%] w-[95%] h-full ">
+          <div className="flex flex-col gap-3 md:w-[43%] w-[80%] md:mr-5 h-full ">
             <p className="font-semibold text-xl  text-center md:text-start">
               Apple iPhone 14 Pro Max
             </p>
@@ -180,7 +195,7 @@ const Details = () => {
               />
             </div>
             <div className="w-[100%]">
-              <p className="text-[12px] text-[#6c6c6c] md:w-[100%] w-[72%] mx-auto">
+              <p className="text-[12px] text-[#6c6c6c] md:w-[100%] w-[90%] mx-auto">
                 Enhanced capabilities thanks to an enlarged display of 6.7
                 inches and work without recharging throughout the day.
                 Incredible photos in weak yes and in bright light using the new
@@ -188,10 +203,10 @@ const Details = () => {
               </p>
             </div>
             <div className="flex justify-between flex-col md:flex-row items-center space-y-5 md:space-y-0">
-              <button className="bg-white md:w-[48%] w-[75%] h-[45px] rounded-[4px] border border-black text-[13px]">
+              <button className="bg-white md:w-[48%] w-[90%] h-[45px] rounded-[4px] border border-black text-[13px]">
                 Add To WishList
               </button>
-              <button className="bg-black text-white md:w-[48%] w-[75%] h-[45px] rounded-[4px] text-[13px]">
+              <button className="bg-black text-white md:w-[48%] w-[90%] h-[45px] rounded-[4px] text-[13px]">
                 Add To Card
               </button>
             </div>
@@ -217,8 +232,8 @@ const Details = () => {
         </div>
       </div>
 
-      <div className="w-full h-[100%] bg-red-200">
-        <div className="max-w-[1250px] h-[500px] mx-auto mt-[60px] px-10 bg-green-200">
+      <div>
+        <div className="max-w-[1250px] rounded lg:mx-auto mt-[60px] mx-5 px-10 bg-white">
           <h4 className="py-5 text-lg">Details</h4>
           <p className="text-[13px] text-[#9d9d9d]">
             Just as a book is judged by its cover, the first thing you notice
@@ -233,6 +248,32 @@ const Details = () => {
             for many.
           </p>
           <p className="py-5">Screen</p>
+
+          <div>
+            <Screens text1={"Screen diagonal"} text2={'6.7"'} />
+            <Screens text1={"The screen resolution"} text2={"2796x1290"} />
+            <Screens text1={"The screen refresh rate"} text2={"120 Hz"} />
+            <Screens text1={"The pixel density "} text2={"460 ppi"} />
+            <Screens text1={"Screen type "} text2={"OLED"} />
+            <Screens
+              text1={"Additionally "}
+              text2={"Dynamic Island"}
+              text3={"Always-on display"}
+              text4={"HDR display"}
+              text5={"True Tone"}
+              text6={"Wide Color (P3)"}
+            />
+            <Screens text1={"CPU "} text2={"A16 Bionic"} header={"CPU"} />
+            <div className="flex justify-between">
+              <p className="text-[13px]">Number of cores</p>
+              <p className="text-[13px]">6</p>
+            </div>
+          </div>
+          <div className="flex justify-center items-center mt-5 pb-10">
+            <button className="h-[40px] w-[140px] pl-3 rounded-md  border-black border bg-white text-[13px] flex items-center justify-center">
+              View More <MdOutlineArrowDropDown className="text-[25px]" />
+            </button>
+          </div>
         </div>
       </div>
     </div>
